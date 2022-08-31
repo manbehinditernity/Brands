961c24fb79e95bad92afd71f33529df9aeda9412
Iternity <86179987+manbehinditernity@users.noreply.github.com>
2022-08-31 10:24:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const img = document.querySelector("img");
+const cursor = document.querySelector("#cursor");
+
+img.addEventListener("mousemove", function(dets){
+    const rotx = 58 - dets.x/11;
+    const roty = Math.abs(rotx);
+    img.style.transform = `rotateX(${rotx}deg)` `rotateY(${-roty}deg)`
+})
+
+img.addEventListener("mouseleave", function(dets){
+    setTimeout(function(){
+    img.style.transform = `rotateX(0deg)` `rotateY(0deg)`
+    })
+    },1000)
+
+document.addEventListener("mousemove", function(dets){
+    cursor.style.left = `${dets.x}px`
+    cursor.style.top = `${dets.y}px`
+
+})   
